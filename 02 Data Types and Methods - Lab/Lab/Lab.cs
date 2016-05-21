f10f140db590b24e78b1c14fc9fdf827d0abf7e7
Anna <annsta@gmail.com>
2016-05-21 14:50:54 +0300

    Programming Fundamentals

---
+++ 02 Data Types and Methods - Lab/Lab/Lab.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab
+{
+    class Lab
+    {
+        static void Main(string[] args)
+        {
+            byte centuries = 0;
+            for (int i = 0; i < 300; i++)
+            {
+                checked
+                {
+                    centuries++;
+                    Console.WriteLine(centuries);
+                }
+            }
+        }
+    }
+}
