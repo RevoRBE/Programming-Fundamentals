301ba6e53b12f227f4f839b2c671bac415efc4d7
Anna <annsta@gmail.com>
2016-06-01 00:41:02 +0300

    Programming Fundamentals

---
+++ 04 Lists and Matrices - Exercises/Demo/Demo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Demo
+{
+    public class Demo
+    {
+        public static void Main()
+        {
+            List<int> list = new List<int>();
+            list.Add(0);           
+            list.Add(0);
+
+            list.Add(1);
+            list.RemoveAll(x => x == 0);    // NB!
+
+            Console.WriteLine(string.Join(" ", list));
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
