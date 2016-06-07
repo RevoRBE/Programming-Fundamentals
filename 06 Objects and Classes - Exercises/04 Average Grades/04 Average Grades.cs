1b596b6deb37d76478024a69e4379d52bcebe3cd
Anna <annsta@gmail.com>
2016-06-07 19:48:51 +0300

    Programming Fundamentals

---
+++ 06 Objects and Classes - Exercises/04 Average Grades/04 Average Grades.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Average_Grades
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public List<int> Grades { get; set; }
+        public double AverageGrade
+        {
+            get
+            { return (double)Grades.Sum() / Grades.Count; }
+        }
+    }
+
+    public class Average_Grades
+    {
+        public static void Main()
+        {
+            Student[] studentGrades = GetStudentGrades();
+            string[] topStudents = studentGrades
+                                    .Where(x => x.AverageGrade >= 5)
+                                    .Select(x => x.Name).Distinct()
+                                    .OrderBy(x => x)
+                                    .ToArray();
+            foreach (string student in topStudents)
+            {
+                double[] studentAverageGrades = studentGrades
+                                                .Where(x => x.Name == student && x.AverageGrade >= 5)
+                                                .Select(x => x.AverageGrade)
+                                                .ToArray();
+                foreach (double studentAverageGrade in studentAverageGrades)
+                    Console.WriteLine("{0} -> {1:f2}", student, studentAverageGrade);
+            }
+        }
+
+        private static Student[] GetStudentGrades()
+        {
+            int n = int.Parse(Console.ReadLine());
+            Student[] grades = new Student[n];
+            for (int i = 0; i < n; i++)
+            {
+                List<int> studentGrades = new List<int>();
+                string[] data = Console.ReadLine()
+                                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                for (int j = 1; j < data.Length; j++)
+                    studentGrades.Add(int.Parse(data[j]));
+                grades[i] = new Student() { Name = data[0], Grades = studentGrades };
+            }
+            return grades;
+        }
+    }
+}
