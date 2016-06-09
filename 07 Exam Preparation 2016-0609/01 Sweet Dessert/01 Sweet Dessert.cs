16332c86026b209d6d09062724310bbc8abf1ca1
Anna <annsta@gmail.com>
2016-06-10 00:30:42 +0300

    Programming Fundamentals Sample Exam I

---
+++ 07 Exam Preparation 2016-0609/01 Sweet Dessert/01 Sweet Dessert.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Sweet_Dessert
+{
+    public class Sweet_Dessert
+    {
+        public static void Main()
+        {
+            decimal cash = decimal.Parse(Console.ReadLine());
+            int guests = int.Parse(Console.ReadLine());
+            decimal[] prices = new decimal[3]; // bananas, eggs, berries (kg)
+            for (int i = 0; i < 3; i++)
+                prices[i] = decimal.Parse(Console.ReadLine());
+            int sets = (int)Math.Ceiling(guests / 6.0);
+            decimal expenses = (2 * prices[0] + 4 * prices[1] + 0.2m * prices[2]) * sets;
+            if (expenses <= cash)
+                Console.WriteLine($"Ivancho has enough money - it would cost {expenses:f2}lv.");
+            else
+                Console.WriteLine("Ivancho will have to withdraw money - he will need {0:f2}lv more.", expenses - cash);
+        }
+    }
+}
---
+++ 07 Exam Preparation 2016-0609/02 Array Modifier/02 Array Modifier.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace Array_Modifier
+{
+    public class Array_Modifier
+    {
+        public static void Main()
+        {
+            long[] numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(long.Parse).ToArray();
+            string input = "";
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] data = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                string command = data[0];
+                switch (command)
+                {
+                    case "swap":
+                        {
+                            int index1 = int.Parse(data[1]);
+                            int index2 = int.Parse(data[2]);
+                            if (index1 >= 0 && index1 < numbers.Length &&
+                                index2 >= 0 && index2 < numbers.Length)
+                            {
+                                long temp = numbers[index1];
+                                numbers[index1] = numbers[index2];
+                                numbers[index2] = temp;
+                            }
+                            break;
+                        }
+                    case "multiply":
+                        {
+                            int index1 = int.Parse(data[1]);
+                            int index2 = int.Parse(data[2]);
+                            if (index1 >= 0 && index1 < numbers.Length &&
+                                index2 >= 0 && index2 < numbers.Length)
+                                numbers[index1] *= numbers[index2];
+                            break;
+                        }
+                    case "decrease":
+                        {
+                            for (int i = 0; i < numbers.Length; i++)
+                                numbers[i]--;
+                            break;
+                        }
+                }
+            }
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
